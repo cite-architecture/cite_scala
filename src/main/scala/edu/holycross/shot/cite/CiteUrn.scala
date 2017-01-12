e77239ebbd1bcbabeec85ebfa303fd5ec9487125
Neel Smith <dnsmith.neel@gmail.com>
2017-01-12 14:19:47 -0500

    Clean up prior to rewokring ofr CITE2 URNs

--- src/main/scala/edu/holycross/shot/cite/CiteUrn.scala
+++ src/main/scala/edu/holycross/shot/cite/CiteUrn.scala
@@ -3,16 +3,14 @@ package edu.holycross.shot
 package cite {
 
 
-
-
-
-  /** A URN for a canonically text or passage of text.
+  /** A URN for a citable object in a collection.
   *
   * @constructor create a new CiteUrn
   * @param urnString String representation of CiteUrn validating
-  * againt the CtsUrn specification
+  * against the CiteUrn specification
   */
-  case class CiteUrn  (val urnString: String) extends Urn {
+  case class Cite2Urn (val urnString: String) extends Urn {
+
     /** Array of four top-level, colon-delimited components.
     */
     val components = urnString.split(":")
@@ -24,22 +22,21 @@ package cite {
     require(components(1) == "cite", "invalid URN syntax: " + urnString + ". Second component must be 'cite'.")
 
     /** Required namespace component of the URN.*/
-    def namespace = components(2)
+    val namespace = components(2)
 
     /** Required work component of the URN.*/
-    def objectComponent = components(3)
+    val objectComponent = components(3)
 
-    /** Array of dot-separated parts of the objectComponent.*/
-    def objectParts = objectComponent.split("""\.""")
-    require((objectParts.size < 4) && (objectParts.size > 0), "invalid number of parts in object component " + objectComponent + " - " + objectParts.size)
+    val dotParts = objectComponent.split("""\.""")
+    val collection = dotParts(0)
+    require((dotParts.size < 4) && (dotParts.size > 0), "invalid number of parts in object component " + objectComponent + " - " + dotParts.size)
 
-    def collection = objectParts(0)
+    val objectString = dotParts.tail.mkString(".")
+    val objectParts = objectString.split("-")
 
 
-    /** Object part of work hierarchy.
-    *
-    * Value is an empty string if there is no work part.
-    */
+
+    /*
     def objOption: Option[String] = {
       objectParts.size match {
         case 2 => Some(objectParts(1))
@@ -54,6 +51,8 @@ package cite {
         case e: java.util.NoSuchElementException => throw CiteException("No object defined in " + urnString)
       }
     }
+
+*/
 
     /** Version part of work hierarchy.
     *
@@ -74,10 +73,125 @@ package cite {
     }
 
 
-
     // Still need to add subreff on single nodes
+    def objectSubrefOption: Option[String] = {
+      try {
+        subrefOption(passageNode)
+      } catch {
+        case e: java.util.NoSuchElementException => None
+        case citeEx: CiteException => None
+        case otherEx : Throwable => throw( otherEx)
+      }
+    }
+
+    def objectSubref = {
+      try {
+        objectSubrefOption.get
+      } catch {
+        case e: java.util.NoSuchElementException => throw CiteException("No individual node subref defined in " + urnString)
+        case otherEx : Throwable => throw( otherEx)
+      }
+    }
     // Still need to add range URNs
     // Still need to add subreff on range nodes
+
+    def rangeBeginOption: Option[String] = {
+      if (passageParts.size > 1) Some(passageParts(0)) else None
+    }
+    def rangeBegin = {
+      try {
+        rangeBeginOption.get
+      } catch {
+        case e: java.util.NoSuchElementException => throw CiteException("No range beginning defined in " + urnString)
+        case otherEx : Throwable => throw( otherEx)
+      }
+    }
+    def rangeBeginParts = {
+      rangeBeginOption match {
+        case None => Array.empty[String]
+        case _ => rangeBegin.split("@")
+      }
+    }
+    def rangeBeginRefOption: Option[String] = {
+      if (rangeBeginParts.isEmpty) None else Some(rangeBeginParts(0))
+    }
+    def rangeBeginRef = {
+      try {
+        rangeBeginRefOption.get
+      } catch {
+        case e: java.util.NoSuchElementException => throw CiteException("No range beginning reference defined in " + urnString)
+        case otherEx : Throwable => throw( otherEx)
+      }
+    }
+
+    /** Full string value of the range beginning's subref.*/
+    def rangeBeginSubrefOption = {
+      rangeBeginOption match {
+        case None => None
+        case _ =>  subrefOption(rangeBegin)
+      }
+    }
+    def rangeBeginSubref = {
+      try {
+        rangeBeginSubrefOption.get
+      } catch {
+        case e: java.util.NoSuchElementException => throw CiteException("No range beginning subreference defined in " + urnString)
+        case otherEx : Throwable => throw( otherEx)
+      }
+    }
+
+
+
+    /** Second range part of the passage component of the URN.
+    *
+    * Value is an empty string if there is no passage component
+    * or if the passage component is a node reference.
+    */
+    def rangeEndOption: Option[String] = {
+      if (passageParts.size > 1) Some(passageParts(1)) else None
+    }
+    def rangeEnd = {
+      try {
+        rangeEndOption.get
+      } catch {
+        case e: java.util.NoSuchElementException => throw CiteException("No range ending defined in " + urnString + " from passage parts " + passageParts.toVector)
+        case otherEx : Throwable => throw( otherEx)
+      }
+    }
+    def rangeEndParts = {
+      rangeEndOption match {
+        case None => Array.empty[String]
+        case _ => rangeEnd.split("@")
+      }
+    }
+    def rangeEndRefOption: Option[String] = {
+      if (rangeEndParts.isEmpty) None else Some(rangeEndParts(0))
+    }
+    def rangeEndRef = {
+      try {
+        rangeEndRefOption.get
+      } catch {
+        case e: java.util.NoSuchElementException => throw CiteException("No range ending reference defined in " + urnString)
+        case otherEx : Throwable => throw( otherEx)
+      }
+    }
+
+    /** Full string value of the range Endning's subref.*/
+    def rangeEndSubrefOption = {
+      rangeEndOption match {
+        case None => None
+        case _ =>  subrefOption(rangeEnd)
+      }
+    }
+    def rangeEndSubref = {
+      try {
+        rangeEndSubrefOption.get
+      } catch {
+        case e: java.util.NoSuchElementException => throw CiteException("No range ending subreference defined in " + urnString)
+        case otherEx : Throwable => throw( otherEx)
+      }
+    }
+
 
 
 
--- src/main/scala/edu/holycross/shot/cite/Models.scala
+++ src/main/scala/edu/holycross/shot/cite/Models.scala
@@ -3,7 +3,6 @@ package edu.holycross.shot
 
 package cite {
 
-
   /** The superclass of Urn objects, implemented by CtsUrn and CiteUrn.
   */
   abstract class Urn {}
@@ -11,33 +10,10 @@ package cite {
   /** Trait for any citable scholarly resource.
   *
   * Implementing classes must have a Urn value
-  * identifying the object.  An example in the cite
-  * library is the Orca class.
+  * identifying the object.
   */
   trait Citable {
     val urn: Urn
   }
 
-  trait Orca extends Citable {
-    /** Identifier for the Orca object.*/
-    val urn: Urn
-
-    /** Identifier for the passage of text analyzed.
-    *
-    * Rdf verbs orca/rdf/analyzes <-> analyzedBy
-    */
-    val passage: CtsUrn
-
-    /** Identifier for the result of the analysis.
-    *
-    * Rdf verbs orca/rdf/hasAnalysis <-> analysisFor
-    */
-    val analysis: CiteUrn
-
-    /** Text reading or deformation resulting from the analysis.
-    *
-    * Rdf verb -> orca/rdf/textDeformation
-    */
-    val deformation: String
-  }
 }
